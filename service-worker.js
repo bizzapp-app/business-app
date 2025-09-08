5bd62629303a26cc51183ad18789471849b19024
bizzapp-app <brianmurayandungu@gmail.com>
2025-09-08 07:40:57 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,31 @@
-const CACHE_NAME = 'smartbizz-cache-v1';
+const CACHE_NAME = 'smartbizz-pos-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
   '/icon-192.png',
   '/icon-512.png',
-  // Add all .js, .css files you reference, eg:
-  '/registerServiceWorker.js',
-  // Add library CDNs you use, eg:
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
-// Install event: cache files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll(urlsToCache)
-    )
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event: clean old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
-    )
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
 });
 
-// Fetch event: serve cached assets when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
   );
 });
